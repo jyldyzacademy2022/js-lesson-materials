fa116a37efbc4e8ee6ea5a106d9fa916328242b6
jyldyzacademy2022 <jyldyzacademy.lms@gmail.com>
2022-01-12 22:41:27 +0600

    add lesson 2

---
+++ 02-basics/js/script.js
@@ -0,0 +1,67 @@
+// const souvenir = [];
+
+// souvenir.push("hello");
+
+// console.log(souvenir);
+
+// const PI = Math.E;
+// console.log(PI);
+
+// let myFloat = -654654;
+
+// let rounded = Math.ceil(myFloat);
+// let rounded = Math.floor(myFloat);
+// let rounded = Math.round(myFloat);
+// let rounded = Math.abs(myFloat);
+// console.log(rounded);
+
+// console.log(Math.max(1, 34, 2, 5, 450));
+// console.log(Math.min(1, 34, 2, 5, 450, -2));
+// console.log(Math.pow(2, 4));
+//let myRandom = Math.floor(Math.random() * 10 + 1);
+
+// console.log(Math.sqrt(8));
+// 012
+// let str = "mir";
+
+// let lastChar = str[str.length - 1];
+
+// console.log(lastChar);
+
+// let fullName = firstName.concat(" ", secondName);
+
+// console.log(firstName.repeat(5));
+
+// let sentence = "The quick brown fox jumps over the lazy dog.";
+
+// console.log(sentence.slice(5, 8));
+// console.log(sentence.substring(5, 8));
+
+// let firstName = "John ";
+
+// console.log(firstName.trim());
+// let firstChar = firstName[0].toUpperCase();
+// let theRest = firstName.slice(1);
+
+// let formattedFirstName = firstChar + theRest;
+// console.log(formattedFirstName);
+
+// firstName.toUpperCase();
+// console.log(firstName);
+
+// let secondName = "doe";
+
+// let userName = "Vasya";
+
+// let message = `Hello, ${4 - 5}`;
+
+// console.log(message);
+
+let friends = ["john", "vasya", "marry"];
+
+friends.push("pete");
+
+console.log(friends);
+
+friends.pop();
+console.log(friends);
