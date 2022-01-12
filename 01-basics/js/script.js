c50145780a788d9a3fe5b2d025125e0e714c0dda
jyldyzacademy2022 <jyldyzacademy.lms@gmail.com>
2022-01-12 22:40:48 +0600

    add lesson 1

---
+++ 01-basics/js/script.js
@@ -0,0 +1,108 @@
+// ;askldjf;asdklfj;klj;
+
+/*
+
+asd;klfja;sdklf
+
+;askdlfj;
+;aksdjf;
+;aslkdjf
+;askdjf
+
+*/
+
+// alert("Hello World!");
+// alert("Hello World 2!");
+// alert("Hello World 3!");
+// alert("Hello World 4!");
+
+// console.log("Hello World!");
+// console.log("Hello World 2!");
+// console.log("Hello World 3!");
+
+// var user = "10";
+// console.log(user);
+
+// user = 20;
+// console.log(user);
+
+// let user = "Mirlan";
+// console.log(user);
+
+// userAge
+// 12user
+// user1
+// user_name
+
+// $
+
+// let $ = 10;
+// console.log($);
+
+// _user
+
+// userName
+
+/* Типы данных */
+// primitives ---> примитивные (простые)
+// string
+// let userName = "Mirlan 'mirlan' ";
+// console.log(userName);
+// userName = "Mirlan";
+// userName = `Mirlan`;
+
+// number
+// let a = 50;
+// let b = 2.2;
+// console.log(a % b);
+
+// boolean
+// let isMarried = false;
+// console.log(isMarried);
+
+// null
+// let car = null;
+// car = 'BMW';
+
+// undefined
+// let car;
+
+// symbol
+// let symbol = Symbol();
+
+// reference types --> сложные типы данных
+// array
+// let friends = ["masha", 20, "john", true, ["m", "a", 52]];
+// console.log(friends);
+
+// objects
+// functions
+
+// data
+// let today = new Date();
+// console.log(today);
+
+// let a = "20";
+// console.log(typeof a);
+// a = String(a);
+
+// let b = 20;
+// b = String(b);
+
+// console.log(a + b);
+
+// let a = "20";
+// a = Number(a);
+// console.log(a);
+
+// let userName;
+// userName = prompt("Как тебя зовут?", "Unknown");
+
+// alert("Hello " + userName);
+
+// let a = Number(prompt("Введите длину комнаты:"));
+
+// let b = Number(prompt("Введите ширину комнаты:"));
+
+// let result = a * b;
+// alert("Площадь составляет: " + result);
