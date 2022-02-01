1c8e245cc7657148518eff0d56996a1970377ede
jyldyzacademy2022 <jyldyzacademy.lms@gmail.com>
2022-02-01 09:03:40 +0600

    lesson 5

---
+++ 05-basics/js/script.js
@@ -0,0 +1,39 @@
+// Global Scope
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+console.log("script1", a, b, c);
+
+// console.log("Global: ", a, b, c);
+
+// // Function scope
+// function test() {
+//   var a = 100;
+//   let b = 200;
+//   const c = 300;
+
+//   console.log("Function scope:", a, b, c);
+// }
+
+// test();
+// console.log("Global: ", a, b, c);
+
+// // Block scope
+// if (true) {
+//   var a = 1;
+//   let b = 2;
+//   const c = 3;
+
+//   console.log("Block scope:", a, b, c);
+// }
+
+// console.log("Global: ", a, b, c);
+
+// // loop scope
+// for (var a = 5; 5 < 5; a++) {
+//   console.log("loop:", a);
+// }
+
+// console.log("Global: ", a, b, c);
---
+++ 05-basics/js/script2.js
@@ -0,0 +1,5 @@
+var a = 10;
+let b = 20;
+const c = 30;
+
+console.log("scrip2", a, b, c);
