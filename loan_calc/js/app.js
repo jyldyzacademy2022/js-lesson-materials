1b74e0f7f8a704dec999858bd98e1c6d0d470aff
jyldyzacademy2022 <jyldyzacademy.lms@gmail.com>
2022-02-04 22:21:13 +0600

    loan calculator not finished

---
+++ loan_calc/js/app.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("#loan-form");
+
+form.addEventListener("submit", calculateResults);
+
+function calculateResults(e) {
+  const amount = document.querySelector("#amount");
+  const interest = document.querySelector("#interest");
+  const years = document.querySelector("#years");
+
+  const monthlyPayment = document.querySelector("#monthly-payment");
+  const totalPayment = document.querySelector("#total-payment");
+  const totalInterest = document.querySelector("#total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    alert("Заполните все поля!");
+  }
+
+  e.preventDefault();
+}
