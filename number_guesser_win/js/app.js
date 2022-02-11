9d1bb56b453d045be17de1f7306fafc019f1b0ac
jyldyzacademy2022 <jyldyzacademy.lms@gmail.com>
2022-02-11 21:19:48 +0600

    numberguesser finish

--- number_guesser_win/js/app.js
+++ number_guesser_win/js/app.js
@@ -1,6 +1,6 @@
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 const game = document.querySelector("#game"),
@@ -23,13 +23,58 @@ guessBtn.addEventListener("click", function () {
 
   // check if won
   if (guess === winningNum) {
-    guessInput.disabled = true;
+    /* guessInput.disabled = true;
     guessInput.style.border = "1px solid green";
-    setMessage(`Поздравляю! Вы угадали число ${winningNum}`, "green");
+    setMessage(`Поздравляю! Вы угадали число ${winningNum}`, "green"); */
+
+    gameOver(true, `Поздравляю! Вы угадали число ${winningNum}`);
+  } else {
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+      // lost
+      /*  guessInput.disabled = true;
+      guessInput.style.border = "1px solid red";
+      setMessage(`Вы проиграли! Правильный ответ ${winningNum}`, "red"); */
+
+      gameOver(false, `Вы проиграли! Правильный ответ ${winningNum}`);
+    } else {
+      // continues
+
+      guessInput.style.border = "1px solid red";
+
+      guessInput.value = "";
+      setMessage(`Неверно! У вас осталось ${guessesLeft} попыток`, "red");
+    }
   }
 });
+
+// play again
+game.addEventListener("mousedown", function (e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+
+  guessInput.disabled = true;
+  guessInput.style.border = `1px solid ${color}`;
+  message.style.color = color;
+  setMessage(msg);
+
+  // play again
+  guessBtn.textContent = "Начать заново";
+  guessBtn.className += "play-again";
+}
 
 function setMessage(msg, color) {
   message.textContent = msg;
   message.style.color = color;
+}
+
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
