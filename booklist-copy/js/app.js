2685a1a455c0a607ade86bb2012605baa0d39d2b
jyldyzacademy2022 <jyldyzacademy.lms@gmail.com>
2022-02-14 22:14:57 +0600

    booklist start

---
+++ booklist-copy/js/app.js
@@ -0,0 +1,25 @@
+/* Book Constructor */
+function Book() {}
+
+/* UI Constructor  */
+function UI() {}
+
+/* UI Prototype Methods */
+UI.prototype.addBookToList = function (book) {
+  // Find the table
+  // Create tr element
+  // Insert cols
+  // Append to table
+};
+
+// Clear fields
+UI.prototype.clearFields = function () {};
+
+/* Event Listeners */
+document.getElementById("book-form").addEventListener("submit", (e) => {
+  // Get form values
+  // Instance of book
+  // Instance of UI
+  // Add book to list
+  // Clear fields
+});
